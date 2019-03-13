2c59b2068fb917bdc61423df8f06bf70e9dd497b
Leonis <tv-games@mail.ru>
2019-03-13 14:36:54 +0200

    Simple test

--- src/test/java/md/leonis/assistant/MainAppTests.java
+++ src/test/java/md/leonis/assistant/MainAppTests.java
@@ -1,7 +1,9 @@
 package md.leonis.assistant;
 
+import md.leonis.assistant.service.SampleService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MainAppTests {
 
+    @Autowired
+    private SampleService sampleService;
+
     @Test
     public void contextLoads() {
+        sampleService.echo();
     }
 
 }
